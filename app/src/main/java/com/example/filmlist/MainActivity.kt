53640a4eacd0039ff9d8103e9cfea09b9c78e611
Jacob Viveros <77998206+YakosWorkshop@users.noreply.github.com>
2025-01-01 20:52:01 -0600

    Initial commit

---
+++ app/src/main/java/com/example/filmlist/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.filmlist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.filmlist.ui.theme.FilmListTheme
+import kotlinx.serialization.Serializable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FilmListTheme {
+                @Serializable
+                object Profile
+                @Serializable
+                object FriendsList
+
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = Profile) {
+                    composable<Profile> { ProfileScreen( /* ... */ ) }
+                    composable<FriendsList> { FriendsListScreen( /* ... */ ) }
+                    // Add more destinations similarly.
+                }
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FilmListTheme {
+        Greeting("Android")
+    }
+}
